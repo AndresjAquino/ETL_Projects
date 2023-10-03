fb2d33eb464ca39a97c2fea8ac97cc7c3cfbae45
Andrés Aquino <PerroPerron@users.noreply.github.com>
2023-10-03 12:27:26 -0300

    commit

--- airflow_docker/dags/Dag_Entrega3.py
+++ airflow_docker/dags/Dag_Entrega3.py
@@ -6,12 +6,6 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
-default_args={
-    'owner': 'AndresAquino',
-    'retries':5,
-    'retry_delay': timedelta(minutes=3)
-}
-
 # Lista de tickers que deseas consultar
 tickers = ['AAL', 'AAPL', 'GOOGL', 'AMZN', 'MSFT', 'TSLA', 'META', 'NVDA', 'JPM', 'GS']
 api_key = 'a6f7a1b79e3bdcf8cff1abd40b8105cd'
@@ -53,7 +47,7 @@ def exportar_a_base_de_datos():
 
 # Definir el DAG
 default_args = {
-    'owner': 'airflow',
+    'owner': 'AndresAquino',
     'start_date': datetime(2023, 10, 2),
     'retries': 1,
 }
@@ -62,6 +56,10 @@ dag = DAG(
     'cargar_datos_empresas',
     default_args=default_args,
     schedule_interval=None,  # Programar la ejecución según tus necesidades
+    dag_id='Dag_Entrega3',
+    description= '',
+    start_date=datetime(2023,10,1,2),
+    schedule_interval='@daily'
 )
 
 # Tareas del DAG
