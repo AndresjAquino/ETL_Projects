cd1caa6a2fbe18e7976d4d96c8641001b3718f57
Andrés Aquino <PerroPerron@users.noreply.github.com>
2023-10-03 08:12:03 -0300

    commit

--- Entrega2.py
+++ Entrega2.py
@@ -1,48 +1,37 @@
-from fmp_python.fmp import FMP
-import json
-from sqlalchemy import create_engine, text
-from decouple import config
+import requests
 import pandas as pd
+from sqlalchemy import create_engine
+from decouple import config
 
-# Configuración
-API_KEY = 'a6f7a1b79e3bdcf8cff1abd40b8105cd'
-SYMBOLS = ['AAL', 'AAPL', 'GOOGL', 'AMZN', 'MSFT', 'TSLA', 'META', 'NVDA', 'JPM', 'GS']
+# Lista de tickers que deseas consultar
+tickers = ['AAL', 'AAPL', 'GOOGL', 'AMZN', 'MSFT', 'TSLA', 'META', 'NVDA', 'JPM', 'GS']
+api_key = 'a6f7a1b79e3bdcf8cff1abd40b8105cd'
 
-# Función para obtener datos de una empresa y almacenarlos en el diccionario
-def obtener_datos_empresa(api, symbol):
-    data = api.get_quote(symbol)
-    print(f'{symbol}: {data}')
-    return data[0]
+# Inicializar un DataFrame vacío para almacenar los datos
+result_df = pd.DataFrame()
 
-# Diccionario para almacenar datos de las empresas
-empresa_data = {}
+# Configurar la URL base
+base_url = 'https://financialmodelingprep.com/api'
+base_type = 'income-statement'
 
-# Obtener y almacenar datos de cada empresa
-fmp = FMP(api_key=API_KEY)
-for symbol in SYMBOLS:
-    empresa_data[symbol] = obtener_datos_empresa(fmp, symbol)
+# Recorrer los tickers y obtener los datos para cada uno
+for ticker in tickers:
+    url = f'{base_url}/v3/{base_type}/{ticker}?apikey={api_key}'
+    response = requests.get(url)
+    data = response.json()
+    if data:
+        # Crear un DataFrame con los datos y agregar una columna "Ticker" para identificar la empresa
+        df = pd.DataFrame(data)
+        df['Ticker'] = ticker
+        
+        # Concatenar el DataFrame actual con el resultado acumulado
+        result_df = pd.concat([result_df, df], ignore_index=True)
 
-# Guardar datos en un archivo JSON
-with open('empresa_data.json', 'w') as json_file:
-    json.dump(empresa_data, json_file)
+# Mostrar el DataFrame resultante con los datos de todas las empresas
+result_df
 
-# Leer json y cargar en un DataFrame
-with open('empresa_data.json', 'r') as json_file:
-    empresa_data = json.load(json_file)
-
-# Convertir el diccionario en un DataFrame
-df = pd.DataFrame.from_dict(empresa_data).T  # Transponer el DataFrame
-df.index.name = 'symbol'  # Cambiar el nombre del índice
-
-# Columnas deseadas
-columnas = [
-    'symbol', 'name', 'price', 'changesPercentage', 'change',
-    'dayLow', 'dayHigh', 'yearHigh', 'yearLow', 'marketCap',
-    'priceAvg50', 'priceAvg200', 'exchange', 'volume', 'avgVolume',
-    'open', 'previousClose', 'eps', 'pe', 'earningsAnnouncement',
-    'sharesOutstanding', 'timestamp']
-
-df = df[columnas]
+# Guardar el DataFrame en un archivo JSON
+result_df.to_json('datos_empresas.json', orient='records', lines=True)
 
 # Credenciales desde el archivo .env
 username = config('DB_USERNAME')
@@ -51,14 +40,11 @@ host = config('DB_HOST')
 port = config('DB_PORT')
 database_name = config('DB_NAME')
 
-# URL de conexión
-url = f'postgresql://{username}:{password}@{host}:{port}/{database_name}'
-
-# Conexión a la base de datos
-conn = create_engine(url, pool_pre_ping=True)
+# conexión
+engine = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{database_name}')
 
 # Exportar el DataFrame a la base de datos
-df.to_sql('empresa_data', conn, if_exists='replace', index=False)
+result_df.to_sql('datos_empresas', engine, schema='andresjaquino_coderhouse', if_exists='replace', index=False)
 
 # Cierro la conexión a la base de datos
-conn.close()
+engine.dispose()
